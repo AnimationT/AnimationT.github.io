8328b4390093c97dde8c868d6d70480d54645e6c
AnimationT <animationtermination@gmail.com>
2022-03-23 07:31:45 +0000

    Rock Paper Scissors Starting!

--- talent picker/script.js
+++ talent picker/script.js
@@ -1,4 +1,4 @@
-var talents = ["Solve a Rubik's Cube","Sing a Song","An awesome animation", "Laughter Blaster"]
+var talents = ["Solve a Rubik's Cube","My Website!","An awesome animation", "Laughter Blaster"]
 
 $("#magic8Ball").click(function(){
     $("#talentText").text(talents[Math.floor((Math.random()*talents.length))])
